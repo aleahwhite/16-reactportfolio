7646ffd393cb0a8d5a09833df35b3bd969a28f12
Aleah White <aleahwhite55@gmail.com>
2024-04-08 16:52:31 -0500

    added lots of changes, images, dependencies and styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../assets/aboutme.jpeg';
+import image from '../assets/portfolio.jpeg';
 import '../app.css';
 
 const AboutMe = () => {
@@ -18,7 +18,7 @@ const AboutMe = () => {
         Junior Full-Stack Developer in the last weeks of completing a 
         boot camp at the University of Minnesota. I have gained a lot of skills in HTML, CSS, JS, and
         more I am ready to make an new impact in the tech industry. My passion lies in creating innovative tech applications,
-        and I prefer to be working on the back-end of things. My journey so far hasn't just been about coding from scooping ice cream to serving, I've honed my skills in trust, customer service, and 
+        and I prefer to be working on the back-end of things. My journey so far hasn't just been about coding- from scooping ice cream to serving, I've honed my skills in trust, customer service, and 
         workplace dynamics. Though I am new to the tech community my love for the field grows each day. While I 
         can seem shy at first, I quickly warm up thriving in social settings and collaborative projects. As I move 
         forward, my goal is to find a position that allows me to blend my newfound coding skills and passion for building 
--- src/data.jsx
+++ src/data.jsx
@@ -1,11 +1,17 @@
 import weatherimage from './assets/weather.jpeg';
-import pantryimage from './assets/pantry.jpeg';
+import techblogimage from './assets/techblog.jpeg';
 import expensesimage from './assets/expenses.jpeg';
 
 export const projects = [
     {
+        title: "Tech Blog",
+        description: "This tech blog app allows users to sign up, create posts and comment on others posts.",
+        image: techblogimage,
+        link: "https://github.com/aleahwhite/12-techblog"
+    },
+    {
         title: "Expenses Tracker",
-        description: "This full-stack application keeps track of your expenses and shows charts based on spending habits.",
+        description: "This expenses app keeps track of your expenses and shows charts based on spending habits.",
         image: expensesimage,
         link: "https://github.com/salidamaharjan/expenses-tracker"
     },
@@ -14,27 +20,21 @@ export const projects = [
         description: "This is a 6 day weather app that keeps track of your search and tells you the temp and humidity.",
         image: weatherimage,
         link: "https://aleahwhite.github.io/06-weatherdashboard/"
-    },
-    {
-        title: "Pantry Meals & Music",
-        description: "This website takes what you search for and pulls up several recipes for it, along with a fitting song to cook your food to.",
-        image: pantryimage,
-        link: "https://zallen16.github.io/recipe-generator/"
-    },
+    }
 ];
 
 
 export const skills = [
     {
         category: "Front-end Development",
-        list: ["HTML5", "CSS3", "JavaScript", "Typescript", "React", "Handlebars"]
+        list: ["HTML", "CSS",  "React", "Handlebars", "Bootstrap"]
     },
     {
         category: "Back-end Development",
-        list: ["Node.js", "Express", "MongoDB", "MySQL", "GraphQL"]
+        list: ["Node.js", "MySQL","JavaScript", "Typescript","Express", "MongoDB", "GraphQL"]
     },
     {
-        category: "Other Tools",
-        list: ["Git", "REST APIs", "ORM", "MVC"]
+        category: "Other Skills",
+        list: ["Git", "REST APIs", "Organization", "ORM", "VCS", "MVC"]
     }
 ];
